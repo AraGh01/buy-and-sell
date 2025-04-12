73edbab9b91876cf880519b693898ea4998a8c46
Ara <Ghazaryan01@mail.ru>
2025-04-12 17:40:01 +0300

    init commit

---
+++ src/main/java/com/example/butsell/repository/ImageRepository.java
@@ -0,0 +1,6 @@
+package com.example.butsell.repository;
+import com.example.butsell.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
---
+++ src/main/java/com/example/butsell/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.butsell.repository;
+import com.example.butsell.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
